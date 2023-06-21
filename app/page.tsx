9e99c04a540bd19f133bda73a9d3fcfdd5ee84eb
cam93 <48114344+cam93@users.noreply.github.com>
2023-06-21 01:26:41 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Inspirational Quote Generator</title>
+                <meta name='description' content='A project to generate quotes'/>
+                <meta />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={styles.main}>
+
+            </main>
+        </>
+    )
+}
